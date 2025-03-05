1c8943ff4204355b475fcb14244b34a12c8828b9
Eric Yung <eyung@wustl.edu>
2025-03-05 01:34:50 -0600

    material ui and theme configurations

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import { CssBaseline, ThemeProvider } from "@mui/material"
+import { createTheme } from "@mui/material/styles"
+import { useMemo } from "react"
+import { themeSettings } from "./theme"
+
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings), [])
+  return (
+    <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        Hello world!
+      </ThemeProvider>
+    </div>
+  )
+}
+
+export default App
