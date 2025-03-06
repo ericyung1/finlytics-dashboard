3bdb5a5a43c1a72bebb131f433339e6fda07b9df
Eric Yung <eyung@wustl.edu>
2025-03-06 07:14:48 -0600

    added kpi, product, and transaction data to mongodb

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+import kpiRoutes from "./routes/kpi.js";
+import KPI from "./models/KPI.js";
+import Product from "./models/Product.js";
+import productRoutes from "./routes/product.js";
+import { kpis, products } from "./data/data.js";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -17,6 +22,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
+/* ROUTES */
+app.use("/kpi", kpiRoutes);
+app.use("/product", productRoutes);
+
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
 mongoose
@@ -26,5 +35,10 @@ mongoose
     })
     .then(async () => {
         app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+        
+        /* ADD DATA ONE TIME ONLY OR AS NEEDED */
+        // await mongoose.connection.db.dropDatabase();
+        // KPI.insertMany(kpis)
+        // Product.insertMany(products)
     })
     .catch((error) => console.log(`${error} did not connect`));
