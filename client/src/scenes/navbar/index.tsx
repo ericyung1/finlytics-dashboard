9e60b83aadd06e0d386111ac3ea6870b949e2ab1
Eric Yung <eyung@wustl.edu>
2025-10-06 23:43:32 -0500

    refactor: update architecture, dependencies, and code quality

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -4,9 +4,7 @@ import InsightsIcon from '@mui/icons-material/Insights';
 import { Box, Typography, useTheme } from '@mui/material';
 import FlexBetween from '@/components/FlexBetween';
 
-type Props = {}
-
-const Navbar = (props: Props) => {
+const Navbar = () => {
   const { palette } = useTheme();
   const [selected, setSelected] = useState("dashboard");
   return (
